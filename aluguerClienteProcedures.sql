fe631e8334a9df83c024224e799de6648fae7dc4
lucasxdsy <lucasxdsd@gmail.com>
2025-06-01 03:45:43 +0100

    adicionaCliente, não falha quando devia mas n sei pq

--- aluguerClienteProcedures.sql
+++ aluguerClienteProcedures.sql
@@ -39,4 +39,53 @@ BEGIN
 END $$
 DELIMITER ;
 
--- Procedimento para criar um cliente :)
+-- Procedimento para criar um cliente
+DROP PROCEDURE IF EXISTS novoCliente;
+
+DELIMITER $$
+CREATE PROCEDURE novoCliente
+	(IN Nome VARCHAR(75),
+     IN NIF VARCHAR(9),
+	 IN LocalTrabalho VARCHAR(75),
+     IN Rua VARCHAR(75),
+     IN Localidade VARCHAR(75),
+     IN CodigoPostal VARCHAR(10),
+     IN Telefone VARCHAR(20),
+     IN Inicio DATETIME, IN Fim DATETIME, IN Preco INT, IN Multa INT,
+	 IN FuncionarioId INT, IN AutomovelId INT, IN FilOrigem INT, IN FilDestino INT)
+BEGIN
+	DECLARE id INT;
+    DECLARE nrAlugueres INT;
+    
+    START TRANSACTION;
+    
+    INSERT INTO Cliente -- Base para poder criar o aluguer
+    (Nome, NIF, LocalTrabalho, Rua, Localidade, CodigoPostal)
+    VALUES (Nome, NIF, LocalTrabalho, Rua, Localidade, CodigoPostal);
+    
+    SET id = LAST_INSERT_ID(); -- id do último cliente inserido (com auto_increment)
+    
+    CALL novoAluguer(Inicio, Fim, Preco, Multa, id, FuncionarioId, AutomovelId, FilOrigem, FilDestino);
+    
+	SELECT COUNT(*) INTO nrAlugueres
+	FROM Aluguer
+	WHERE ClienteId = id;
+    
+    SELECT nrAlugueres AS 'Número de alugueres';
+    
+    IF nrAlugueres = 0
+    THEN
+        -- Aluguer não criado, apagar cliente e abortar
+        DELETE FROM Cliente WHERE Id = id;
+        ROLLBACK;
+    ELSE
+	INSERT INTO Cliente_Contacto
+		(ClienteId, Telefone)
+		VALUES (id, Telefone);    
+		
+        COMMIT;
+	END IF;
+    
+END $$
+DELIMITER ;
+    
