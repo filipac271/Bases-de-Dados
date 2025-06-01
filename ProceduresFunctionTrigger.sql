402611c10dae3565a65c2f5c49f93b3f2b37fadc
lucasxdsy <lucasxdsd@gmail.com>
2025-06-01 12:18:05 +0100

    Correção do procedure de criar clientes e movimentação para o ficheiro correto

--- ProceduresFunctionTrigger.sql
+++ ProceduresFunctionTrigger.sql
@@ -61,3 +61,103 @@ BEGIN
 END $$
 
 DELIMITER ;
+
+-- Procedimento para criar um aluguer
+DROP PROCEDURE IF EXISTS novoAluguer;
+
+DELIMITER $$
+CREATE PROCEDURE novoAluguer
+	(IN Inicio DATETIME, IN Fim DATETIME, IN Preco INT, IN Multa INT, IN ClienteId INT
+    	, IN FuncionarioId INT, IN AutomovelId INT, IN FilOrigem INT, IN FilDestino INT)
+BEGIN
+	DECLARE filialFuncionario INT; -- Id da filial de um funcionário
+    DECLARE filialAutomovel INT; -- Id da filial de um carro
+	
+    SELECT F.FilialId INTO filialFuncionario
+		FROM Funcionario AS F
+		WHERE F.Id = FuncionarioId;
+        
+	SELECT A.FilialId INTO filialAutomovel
+		FROM Automovel AS A
+		WHERE A.Id = AutomovelId
+			AND A.Estado = 'Disponível';
+
+    SELECT filialFuncionario AS 'Filial do Funcionário', filialAutomovel AS 'Filial do Automóvel';
+
+    IF filialFuncionario = FilOrigem -- Garante que o funcionario trabalha na filial correta
+		AND filialAutomovel = FilOrigem -- Garante que o automovel está na filial correta e está disponível
+    THEN 
+		START TRANSACTION;
+		INSERT INTO Aluguer
+		(DataInicio, DataFim, Preco, Multa, ClienteId, FuncionarioId, AutomovelId, RecolhidoFilialId, DevolvidoFilialId)
+		VALUES (Inicio, Fim, Preco, Multa, ClienteId, FuncionarioId, AutomovelId, FilOrigem, FilDestino);
+        
+        UPDATE Automovel
+		SET Estado = 'Ocupado', FilialId = FilDestino 
+		WHERE Id = AutomovelId;
+        
+        SELECT CONCAT('Alguer criado com id ', LAST_INSERT_ID()) AS Mensagem;
+		
+        COMMIT;
+	ELSE
+		SELECT 'Aluguer não criado' AS Mensagem;
+	END IF;
+END $$
+DELIMITER ;
+
+
+-- Procedimento para criar um cliente
+DROP PROCEDURE IF EXISTS novoCliente;
+
+DELIMITER $$
+CREATE PROCEDURE novoCliente
+	(IN Nome VARCHAR(75),
+     IN NIF VARCHAR(9),
+	 IN LocalTrabalho VARCHAR(75),
+     IN Rua VARCHAR(75),
+     IN Localidade VARCHAR(75),
+     IN CodigoPostal VARCHAR(10),
+     IN Telefone VARCHAR(20),
+     IN Inicio DATETIME, IN Fim DATETIME, IN Preco INT, IN Multa INT,
+	 IN FuncionarioId INT, IN AutomovelId INT, IN FilOrigem INT, IN FilDestino INT)
+BEGIN
+	DECLARE id INT;
+    DECLARE nrAlugueres INT;
+    
+	DECLARE filialFuncionario INT; -- Id da filial de um funcionário
+    DECLARE filialAutomovel INT; -- Id da filial de um carro
+	
+    SELECT F.FilialId INTO filialFuncionario
+		FROM Funcionario AS F
+		WHERE F.Id = FuncionarioId;
+        
+	SELECT A.FilialId INTO filialAutomovel
+		FROM Automovel AS A
+		WHERE A.Id = AutomovelId
+			AND A.Estado = 'Disponível';
+    
+	IF filialFuncionario = FilOrigem AND filialAutomovel = FilOrigem
+    THEN
+		START TRANSACTION;
+    
+		-- É necessário criar 1º a entrada do Cliente antes do Aluguer
+		INSERT INTO Cliente 
+		(Nome, NIF, LocalTrabalho, Rua, Localidade, CodigoPostal)
+		VALUES (Nome, NIF, LocalTrabalho, Rua, Localidade, CodigoPostal);
+		
+		SET id = LAST_INSERT_ID(); -- id do último cliente inserido (por causa do auto_increment)
+		
+		CALL novoAluguer(Inicio, Fim, Preco, Multa, id, FuncionarioId, AutomovelId, FilOrigem, FilDestino);
+		
+        SELECT CONCAT('Cliente criado com id ', id) AS Mensagem;
+        
+        COMMIT;
+	ELSE
+		SELECT 'Cliente não criado pois o aluguer é inválido' AS Mensagem, 
+			filialFuncionario AS 'Filial do Funcionário',
+            filialAutomovel AS 'Filial do Automóvel';
+	END IF;
+    
+END $$
+DELIMITER ;
+    
